9473cc56f038d04286437fdf9547cb0e942c81ca
HedvaldoCosta <hedvaldocostaV.O@hotmail.com>
2021-06-05 20:45:23 -0300

    Update teste.py

--- kc_house/teste.py
+++ kc_house/teste.py
@@ -1,71 +1,38 @@
-# Bibliotecas necessárias
-import streamlit as st
+# Importação das bibliotecas
 import pandas as pd
 import numpy as np
-import folium
-from streamlit_folium import folium_static
-from folium.plugins import MarkerCluster
-import geopandas
-import plotly.express as px
+import streamlit as st
 
 st.set_page_config(layout='wide')
 
 
-# Função para ler o dataset
-@st.cache(allow_output_mutation=True)
+# Extração e transformação dos dados
 def get_data(path):
-    read_data = pd.read_csv(path)
-    return read_data
+    read_path = pd.read_csv(path)
+    return read_path
 
 
-data = get_data('kc_house_data.csv')
+data = get_data('Datasets/kc_house_data.csv')
+# Remoção das colunas desnecessárias para o insight
+data = data.drop(columns=['sqft_above', 'sqft_basement', 'sqft_living15', 'sqft_lot15', 'yr_renovated'])
+# Formatando colunas de tempo
 data['date'] = pd.to_datetime(data['date']).dt.strftime('%d-%m-%Y')
-data['price_m2'] = data['price'] / (data['sqft_lot'] / 10.764)
+data['year'] = np.int64(pd.to_datetime(data['date']).dt.strftime('%Y'))
+data['age'] = data['year'] - data['yr_built']
 
+# Formatando o tipo das colunas
+data['bathrooms'] = np.int64(round(data['bathrooms'] - 0.3))
+data['floors'] = np.int64(round(data['floors'] - 0.3))
+data['sqft_living'] = round(data['sqft_living'] / 10.764, 2)
 
-# Função para ler a url geofile
-@st.cache(allow_output_mutation=True)
-def get_geofile(url):
-    read_geofile = geopandas.read_file(url)
-    return read_geofile
-
-
-geofile = get_geofile('https://opendata.arcgis.com/datasets/83fc2e72903343aabff6de8cb445b81c_2.geojson')
-
-st.title(body='KC_HOUSE')
-st.markdown(body='Welcome to kc house')
-
-geral, tendencia = st.beta_columns((1, 1))
-
-select_zipcode = st.sidebar.multiselect(
-    label='Enter Zipcode',
-    options=data['zipcode'].unique()
+data['level'] = data['price'].apply(
+    lambda x: 0 if (x >= 0) & (x < 321950) else
+    1 if (x >= 321950) & (x < 450000) else
+    2 if (x >= 450000) & (x < 645000) else
+    3
 )
 
-if select_zipcode:
-    data = data.loc[data['zipcode'].isin(select_zipcode), :]
-else:
-    data = data.copy()
+data['waterfront'] = data['waterfront'].apply(lambda x: 'no' if x == 0 else 'yes')
 
-
-geral.header(body='Visão Geral')
-geral.dataframe(
-    data=data.head(10),
-    height=300
-)
-
-# Estatística Descritiva
-num_select = data.select_dtypes(include=['int64', 'float64'])
-media = pd.DataFrame(num_select.apply(np.mean))
-std = pd.DataFrame(num_select.apply(np.std))
-max_value = pd.DataFrame(num_select.apply(np.max))
-min_value = pd.DataFrame(num_select.apply(np.min))
-
-dataframe_estat = pd.concat([media, std, max_value, min_value], axis=1).reset_index()
-dataframe_estat.columns = ['Attributes', 'averrage', 'std', 'max', 'min']
-tendencia.header(body='Tendência central')
-tendencia.dataframe(
-    data=dataframe_estat.head(10),
-    height=300
-)
-
+data = data.drop(data[data['age'] < 0].index, axis=0)
+st.write(data.head())
