b0a57d3f2f2a148ce9e41c26ad296c1f4b3069a9
HedvaldoCosta <hedvaldocostaV.O@hotmail.com>
2021-06-19 14:08:08 -0300

    Update teste.py

--- kc_house/teste.py
+++ kc_house/teste.py
@@ -1,6 +1,10 @@
 import pandas as pd
 import streamlit as st
 import numpy as np
+import plotly.express as px
+import folium
+from streamlit_folium import folium_static
+from folium.plugins import MarkerCluster
 
 data = 'Datasets/kc_house_data.csv'
 
@@ -47,15 +51,20 @@ def load_data():
     return dataframe
 
 
+def load_map(data_map):
+    mapa = folium.Map(location=[data_map['latitude'].mean(), data_map['longitude'].mean()])
+    
+
+
 # Carregar dados
 read_data = load_data()
 
 # SIDEBAR
 ## SIDEBAR DATASET
+df_data = read_data.copy()
 st.sidebar.title('Filtros da tabela')
-
-filter_columns = st.sidebar.multiselect('Selecione as colunas', read_data.columns)
-filter_cep = st.sidebar.multiselect('Selecione os CEPS', read_data['CEP'].unique().tolist())
+filter_columns = st.sidebar.multiselect('Selecione as colunas', df_data.columns)
+filter_cep = st.sidebar.multiselect('Selecione os CEPS', df_data['CEP'].unique().tolist())
 ## SIDEBAR MAPA
 map_data = read_data.copy()
 st.sidebar.title('Filtros do mapa')
@@ -75,22 +84,66 @@ filter_floors = st.sidebar.multiselect('Andares', sorted(set(commercial_data['an
 # APLICAÇÃO DOS FILTROS
 ## FILTROS DATASET
 if (filter_cep != []) & (filter_columns != []):
-    read_data = read_data.loc[read_data['CEP'].isin(filter_cep), filter_columns]
+    df_data = df_data.loc[df_data['CEP'].isin(filter_cep), filter_columns]
 elif (filter_cep != []) & (filter_columns == []):
-    read_data = read_data.loc[read_data['CEP'].isin(filter_cep), :]
+    df_data = df_data.loc[df_data['CEP'].isin(filter_cep), :]
 elif (filter_cep == []) & (filter_columns != []):
-    read_data = read_data.loc[:, filter_columns]
+    df_data = df_data.loc[:, filter_columns]
 ## FILTROS MAPA
 if filter_waterfront:
     map_data = map_data.loc[map_data['beira_mar'].isin([filter_waterfront])]
 elif filter_price:
     map_data = map_data.loc[map_data['preço'] <= filter_price]
 ## FILTROS COMERCIAIS
-yr_built = read_data.loc[read_data['ano_construção'] <= filter_yr_built]
+df_yrbuilt = read_data.copy()
+yr_built = df_yrbuilt.loc[df_yrbuilt['ano_construção'] <= filter_yr_built]
 data_yr_built = yr_built[['ano_construção', 'preço']].groupby('ano_construção').mean().reset_index()
+fig_yr_built = px.line(data_yr_built, x='ano_construção', y='preço')
 
-#corpo
+df_bedrooms = read_data.copy()
+if filter_bedrooms:
+    df_bedrooms = df_bedrooms.loc[read_data['quartos'].isin(filter_bedrooms)]
+data_bedrooms_price = df_bedrooms[['preço', 'quartos']].groupby('quartos').mean().reset_index()
+data_bedrooms_count = df_bedrooms[['ID', 'quartos']].groupby('quartos').count().reset_index()
+data_bedrooms_count.columns = ['quartos', 'qtd']
+fig_bedrooms_mean = px.bar(data_bedrooms_price, x='quartos', y='preço')
+fig_bedrooms_count = px.bar(data_bedrooms_count, x='quartos', y='qtd')
+
+df_bathrooms = read_data.copy()
+if filter_bathrooms:
+    df_bathrooms = df_bathrooms.loc[read_data['banheiros'].isin(filter_bathrooms)]
+data_bathrooms_mean = df_bathrooms[['preço', 'banheiros']].groupby('banheiros').mean().reset_index()
+data_bathrooms_count = df_bathrooms[['ID', 'banheiros']].groupby('banheiros').count().reset_index()
+data_bathrooms_count.columns = ['banheiros', 'qtd']
+fig_bathrooms_mean = px.bar(data_bathrooms_mean, x='banheiros', y='preço')
+fig_bathrooms_count = px.bar(data_bathrooms_count, x='banheiros', y='qtd')
+
+df_floors = read_data.copy()
+if filter_floors:
+    df_floors = df_floors.loc[read_data['andares'].isin(filter_floors)]
+data_floors_mean = df_floors[['preço', 'andares']].groupby('andares').mean().reset_index()
+data_floors_count = df_floors[['ID', 'andares']].groupby('andares').count().reset_index()
+data_floors_count.columns = ['andares', 'qtd']
+fig_floors_mean = px.bar(data_floors_mean, x='andares', y='preço')
+fig_floors_count = px.bar(data_floors_count, x='andares', y='qtd')
+# CORPO
 st.title('Dataset')
 if st.checkbox('Mostrar tabela com os dados'):
-    st.write(read_data.head(100))
+    st.write(df_data.head(100))
 
+load_map(map_data)
+
+st.title('Área comercial')
+st.plotly_chart(fig_yr_built)
+
+bedrooms1, bedrooms2 = st.beta_columns((1, 1))
+bedrooms1.plotly_chart(fig_bedrooms_mean, use_container_width=True)
+bedrooms2.plotly_chart(fig_bedrooms_count, use_container_width=True)
+
+bathrooms1, bathrooms2 = st.beta_columns((1, 1))
+bathrooms1.plotly_chart(fig_bathrooms_mean, use_container_width=True)
+bathrooms2.plotly_chart(fig_bathrooms_count, use_container_width=True)
+
+floors1, floors2 = st.beta_columns((1, 1))
+floors1.plotly_chart(fig_floors_mean, use_container_width=True)
+floors2.plotly_chart(fig_floors_count, use_container_width=True)
